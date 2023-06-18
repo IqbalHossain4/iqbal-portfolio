32e2d7027cfdb5c7b7d680347b5ac465fc171240
iqbalhossain4 <iqbal-hossain@mail.com>
2023-06-18 20:43:49 +0600

    add about

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../component/Banner/Banner";
 import About from "../component/About_me/About";
+import Work from "../component/Works/Work";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <About />
+      <Work />
     </div>
   );
 };
--- src/component/About_me/About.jsx
+++ src/component/About_me/About.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import logo from "../../assets/image.png";
 const About = () => {
   const [showMore, setShowMore] = useState(false);
   const toggleShowMore = () => {
@@ -10,33 +10,45 @@ const About = () => {
     <div className="text-white bg-blue-600 py-4">
       <div className="container">
         <p className="text-zinc-400">about/&gt;</p>
-        <div>
-          <h1>Iqbal Hossain</h1>
-          <p>Web Developer</p>
-          <div>
-            <p>
-              Hi, I'am Iqbal Hossain, I am a Front-end Web developer. I have
-              experience in real-time work I have done so many projects in the
-              past. I developed so many websites using my short knowledge. I
-              love learning new technologies.
-            </p>
-            {showMore ? (
-              <div>
-                <p>
-                  I know every person never knows everything but I am trying to
-                  learn every day new something. Learning is part of my life. I
-                  believe that if I want to do something new then I have to need
-                  to make myself different from any other.
-                </p>
+        <div className="md:flex   items-center justify-evenly w-full">
+          <div className="w-3/4 mx-auto">
+            <h1 className="text-black font-bold">Iqbal Hossain</h1>
+            <p>Web Developer...</p>
+            <div className="text-md">
+              <p>
+                Hi, I'am Iqbal Hossain, I am a Front-end Web developer. I have
+                experience in real-time work I have done so many projects in the
+                past. I developed so many websites using my short knowledge. I
+                love learning new technologies.
+              </p>
+              {showMore ? (
+                <div>
+                  <p>
+                    I know every person never knows everything but I am trying
+                    to learn every day new something. Learning is part of my
+                    life. I believe that if I want to do something new then I
+                    have to need to make myself different from any other.
+                  </p>
+                  <button className="text-black" onClick={toggleShowMore}>
+                    See Less...
+                  </button>
+                </div>
+              ) : (
                 <button className="text-black" onClick={toggleShowMore}>
-                  See Less
+                  See More...
                 </button>
-              </div>
-            ) : (
-              <button className="text-black" onClick={toggleShowMore}>
-                See More
-              </button>
-            )}
+              )}
+            </div>
+          </div>
+
+          <div className="md:w-1/2">
+            <div className="w-[200px] h-[200px] bg-black  rounded-full border mx-auto">
+              <img
+                src={logo}
+                className="w-full h-full rounded-full"
+                alt="logo"
+              />
+            </div>
           </div>
         </div>
       </div>
